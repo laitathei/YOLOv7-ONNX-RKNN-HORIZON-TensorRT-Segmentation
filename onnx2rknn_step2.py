f57b350cdead669e47ac5bb83fd3782ab9ea37dc
laitathei <56575156+laitathei@users.noreply.github.com>
2023-08-11 09:43:22 +0800

    Update onnx2rknn_step2.py

--- onnx2rknn_step2.py
+++ onnx2rknn_step2.py
@@ -11,7 +11,7 @@ config_path = "./config"
 result_path = "./result"
 image_path = "./dataset/bus.jpg"
 video_path = "test.mp4"
-video_inference = False
+video_inference = True
 model_name = 'yolov7-seg'
 RKNN_MODEL = f'{model_name}-{input_height}-{input_width}.rknn'
 CLASSES = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis','snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
@@ -54,10 +54,40 @@ if __name__ == '__main__':
         exit(ret)
     print('done')
 
-    image_3c = cv2.imread(image_path)
-    image_4c, image_3c =  preprocess(image_3c, input_height, input_width)
-    outputs = rknn.inference(inputs=[image_3c])
-    print(len(outputs))
-    print(outputs[0].shape)
-    print(outputs[0])
+    if video_inference == True:
+        cap = cv2.VideoCapture(video_path)
+        while(True):
+            ret, image_3c = cap.read()
+            if not ret:
+                break
+            print('--> Running model for video inference')
+            image_4c, image_3c = preprocess(image_3c, input_height, input_width)
+            outputs = rknn.inference(inputs=[image_3c])
+            colorlist = gen_color(len(CLASSES))
+            outputs = [outputs[0], outputs[4]]
+            results = postprocess(outputs, image_4c, image_3c, conf_thres, iou_thres) ##[box,mask,shape]
+            results = results[0]              ## batch=1
+            boxes, masks, shape = results
+            if isinstance(masks, np.ndarray):
+                mask_img, vis_img = vis_result(image_3c,  results, colorlist, CLASSES, result_path)
+                cv2.imshow("mask_img", mask_img)
+                cv2.imshow("vis_img", vis_img)
+            else:
+                print("No segmentation result")
+            cv2.waitKey(10)
+    else:
+        image_3c = cv2.imread(image_path)
+        image_4c, image_3c = preprocess(image_3c, input_height, input_width)
+        outputs = rknn.inference(inputs=[image_3c])
+        colorlist = gen_color(len(CLASSES))
+        outputs = [outputs[0], outputs[4]]
+        results = postprocess(outputs, image_4c, image_3c, conf_thres, iou_thres) ##[box,mask,shape]
+        results = results[0]              ## batch=1
+        boxes, masks, shape = results
+        if isinstance(masks, np.ndarray):
+            mask_img, vis_img = vis_result(image_3c,  results, colorlist, CLASSES, result_path)
+            print('--> Save inference result')
+        else:
+            print("No segmentation result")
     rknn.release()
+    cv2.destroyAllWindows()
