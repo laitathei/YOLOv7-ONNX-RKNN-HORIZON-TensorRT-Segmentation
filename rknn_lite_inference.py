d2dc05a31c4c70a7fe7d882a8209da352a2ef593
laitathei <56575156+laitathei@users.noreply.github.com>
2023-08-13 04:57:11 +0800

    Add files via upload

---
+++ rknn_lite_inference.py
@@ -0,0 +1,43 @@
+import os, cv2, time, numpy as np
+from util import *
+from rknnlite.api import RKNNLite
+
+conf_thres = 0.25
+iou_thres = 0.45
+input_width = 640
+input_height = 480
+model_name = 'yolov7-seg'
+model_path = "./model"
+config_path = "./config"
+result_path = "./result"
+image_path = "./dataset/bus.jpg"
+video_path = "test.mp4"
+video_inference = False
+RKNN_MODEL = f'{model_path}/{model_name}-{input_height}-{input_width}.rknn'
+CLASSES = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis','snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+
+if __name__ == '__main__':
+    isExist = os.path.exists(result_path)
+    if not isExist:
+        os.makedirs(result_path)
+    rknn_lite = RKNNLite(verbose=False)
+    ret = rknn_lite.load_rknn(RKNN_MODEL)
+    ret = rknn_lite.init_runtime(core_mask=RKNNLite.NPU_CORE_0_1_2)
+    image_3c = cv2.imread(image_path)
+    image_4c, image_3c = preprocess(image_3c, input_height, input_width)
+    print(image_4c.shape)
+    ret = rknn_lite.init_runtime()
+    outputs = rknn_lite.inference(inputs=[image_4c])
+    outputs[0] = np.squeeze(outputs[0])
+    outputs[0] = np.expand_dims(outputs[0], axis=0)
+    outputs = [outputs[0], outputs[4]]
+    colorlist = gen_color(len(CLASSES))
+    results = postprocess(outputs, image_4c, image_3c, conf_thres, iou_thres) ##[box,mask,shape]
+    results = results[0]              ## batch=1
+    boxes, masks, shape = results
+    if isinstance(masks, np.ndarray):
+        mask_img, vis_img = vis_result(image_3c,  results, colorlist, CLASSES, result_path)
+        print('--> Save inference result')
+    else:
+        print("No segmentation result")
+    rknn_lite.release()
